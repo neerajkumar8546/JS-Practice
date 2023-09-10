19cde78a4a632f52c93340a9eea051e70f25c7fb
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-10 08:34:45 +0000

    Variables

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,15 @@
+const Accountid=50233394587
+let AccountEmail="neerajkumar8546@gmail.com"
+var AccountPassword="Neeraj123"
+AccountCity="AnandNagar"
+let AccountState;
+
+//Accountid=46456
+AccountEmail="neerajk@gmail.com"
+AccountPassword="Neeraj@8546"
+AccountCity="Noida"
+AccountState="Delhi"
+
+console.log(AccountEmail)
+
+console.table([Accountid,AccountEmail,AccountPassword,AccountCity,AccountState])
