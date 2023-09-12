801afb8379798d5c99965dd7508ca456a66773d3
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-12 11:41:28 +0000

    ComparisionOfdatatypes

---
+++ 01_Basic/04_ComparisionOfDatatypes.js
@@ -0,0 +1,9 @@
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0) //null convert to 0 only this operator
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+// === => check value & datatypes
