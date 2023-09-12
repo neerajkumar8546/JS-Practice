4add761b7f75315fcd4708606bc2fd9f5f425a31
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-12 08:38:12 +0000

    DataTypes

---
+++ 01_Basic/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict";   //treat all js code as newer version
+
+//alert(3+3); //we are using node js ,  not browser
+
+console.log(3+
+    3               //code reusebility should be high
+);
+
+console.log("Neeraj")
+let names="neeraj"
+let age=25
+//Primitive datatypes-:
+
+//number =>2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null = standalone value 'its a representation of empty value' ex - temp
+// undefined => 
+// symbol =>unique
+
+//Object
+console.log(typeof names);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined)
