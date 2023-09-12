83367050c7458da03984c32ad6cb5724fa7ee322
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-12 10:45:52 +0000

    ConvertionOperation

---
+++ 01_Basic/03_ConvertionOperation.js
@@ -0,0 +1,57 @@
+//let score=33
+//let score="33abc"
+//let score=null
+//let score=undefined
+let score=true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// if score=null => 0, if score=undifined => NaN
+
+let isLoggedIn=0
+let booleanisLoggedIn=Boolean(isLoggedIn)
+
+//console.log(booleanisLoggedIn);
+
+// 1=>true; 0=>false
+// "" => false
+// "neeraj" => true
+
+let somenumber=33
+let stringNumber=String(somenumber)
+
+//console.log(stringNumber);
+
+// ************************* Operations ***************************//
+
+let value=3
+let negValue=-value
+
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2%2)
+// console.log(2/2)
+
+let str1=2
+let str2=5
+
+console.log(str1+str2)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"4")
+console.log((3+4)*5%3)
+
+
+// str1++ => the increment operator increments and returns the value before incrementing.
+// ++str1 => the increment operator increments and returns the value after incrementing.
