f15c504016a6e208b37cc6c8bca38d9305209614
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-20 12:18:19 +0000

    font color method

---
+++ 01_Basic/String/FontColor_method.js
@@ -0,0 +1,9 @@
+// The fontcolor() method of String values creates a string that embeds this string in a
+//  <font> element (<font color="...">str</font>), which causes this string to be displayed 
+//  in the specified font color.
+
+let Username="NeerajK"
+
+console.log(Username.fontcolor('Green'));
+
+//Using this method embeds a font tag with specific color in this string 
