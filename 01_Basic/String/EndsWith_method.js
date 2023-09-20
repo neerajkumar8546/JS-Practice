bb69ae32959e5ce63b40f3200e529767f02f667b
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-20 11:41:42 +0000

    EndsWith method

---
+++ 01_Basic/String/EndsWith_method.js
@@ -0,0 +1,8 @@
+// The endsWith() method of String values determines whether a string ends with the characters 
+// of this string, returning true or false as appropriate.
+
+let sentence="The example iterates over each character in the string "
+
+console.log(sentence.endsWith('g'))
+console.log(sentence.endsWith('g '))
+console.log(sentence.endsWith('g',54))
