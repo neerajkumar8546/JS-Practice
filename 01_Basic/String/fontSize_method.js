3a7a3b2f821a5f8d6645ce848388ac1b253ddd8a
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-20 12:18:30 +0000

    font size method

---
+++ 01_Basic/String/fontSize_method.js
@@ -0,0 +1,9 @@
+// The fontsize() method of String values creates a string that embeds this string in a <font>
+//  element (<font size="...">str</font>), which causes this string to be displayed in the 
+//  specified font size.
+
+let Username='neerajk'
+
+console.log(Username.fontsize(12));
+
+// using this method create a string embeds with font tag with size attributes
