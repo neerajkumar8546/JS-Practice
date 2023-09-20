bc448fe4ce2658c6f889c65ee86bda0b90ef794e
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-20 11:41:51 +0000

    fixed method

---
+++ 01_Basic/String/fixed_method.js
@@ -0,0 +1,9 @@
+// The fixed() method of String values creates a string that embeds this string in 
+// a <tt> element (<tt>str</tt>), which causes this string to be displayed in a fixed-width font.
+
+let Email="neerajk.ext@innov.in"
+
+console.log(Email.fixed())
+
+//Add tt tag in the string using fixed method , its is used to fixed the font width font
+// of the string 
