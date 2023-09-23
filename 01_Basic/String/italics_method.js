89624bafcd85ff6645d5ba7f24067a63fd04f7b1
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-23 13:26:40 +0000

    italics method ex

---
+++ 01_Basic/String/italics_method.js
@@ -0,0 +1,6 @@
+// The italics() method of String values creates a string that embeds this string in an <i> 
+// element (<i>str</i>), which causes this string to be displayed as italic.
+
+let EmailId="neerajkumar8546@gmail.com"
+
+console.log(EmailId.italics())
