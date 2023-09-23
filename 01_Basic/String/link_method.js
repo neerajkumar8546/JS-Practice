f88a833216b31636d9845b5ed108da5cffb35210
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-23 14:00:00 +0000

    link method ex

---
+++ 01_Basic/String/link_method.js
@@ -0,0 +1,5 @@
+// The link() method of String values creates a string that embeds this string in an <a> element
+//  (<a href="...">str</a>), to be used as a hypertext link to another URL.
+
+let LinkName="Google"
+console.log(LinkName.link("www.google.co.in"));
