3bb06d4c226c851841c1d694e18f55b861aebc24
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-23 09:41:17 +0000

    include method

--- 01_Basic/String/includes_method.js
+++ 01_Basic/String/includes_method.js
@@ -3,7 +3,7 @@
 
 const sentence = 'The quick brown fox jumps over the lazy dog.';
 
-const word = 'Fox';
+const word = 'fox';
 
 console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
 // Expected output: "The word "fox" is in the sentence"
