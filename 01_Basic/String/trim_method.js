cb88bcc91f991b4e9970a2948f211f5957afc862
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 13:53:59 +0000

    trim method

---
+++ 01_Basic/String/trim_method.js
@@ -0,0 +1,6 @@
+//The trim() method of String values removes whitespace from both ends of this string and 
+//returns a new string, without modifying the original string.
+
+const greeting = '   Hello world!   ';
+
+console.log(`Trimed String:${greeting.trim()}`);
