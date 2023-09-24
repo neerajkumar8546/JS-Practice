86bd89dc8a5dc6f5a8ca840c9600c00ab129476d
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 10:15:56 +0000

    startWith method

---
+++ 01_Basic/String/startWith_method.js
@@ -0,0 +1,10 @@
+// The startsWith() method of String values determines whether this string begins with the characters 
+// of a specified string, returning true or false as appropriate.
+
+const str1 = 'Saturday night plans';
+
+console.log(str1.startsWith('Sat'));
+// Expected output: true
+
+console.log(str1.startsWith('Sat',1));
+// Expected output: false
