068f515ab3b23b467f24bda71313251f0f29aed6
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 08:33:48 +0000

    slice method

---
+++ 01_Basic/String/slice_method.js
@@ -0,0 +1,18 @@
+//The slice() method of String values extracts a section of this string and returns it as a new 
+// string, without modifying the original string.
+
+// It is used to extract a portion of an array or string and return it as a new array or string without modifying the original. The method takes one or two arguments: Start & End position
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str.slice(31));
+// Expected output: "the lazy dog."
+
+console.log(str.slice(4, 19));
+// Expected output: "quick brown fox"
+
+console.log(str.slice(-4));
+// Expected output: "dog."
+
+console.log(str.slice(-9, -5));
+// Expected output: "lazy"
