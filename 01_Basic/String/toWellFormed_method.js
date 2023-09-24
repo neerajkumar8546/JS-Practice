77c2848d612e031392a97425747eaed07eb2c2d2
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 13:51:54 +0000

    isWellFormed method

---
+++ 01_Basic/String/toWellFormed_method.js
@@ -0,0 +1,26 @@
+//The toWellFormed() method of String values returns a string where all lone surrogates of this
+// string are replaced with the Unicode replacement character U+FFFD.
+
+const strings = [
+    // Lone leading surrogate
+    "ab\uD800",
+    "ab\uD800c",
+    // Lone trailing surrogate
+    "\uDFFFab",
+    "c\uDFFFab",
+    // Well-formed
+    "abc",
+    "ab\uD83D\uDE04c",
+  ];
+  
+  for (const str of strings) {
+    console.log(str.toWellFormed());
+  }
+  // Logs:
+  // "ab�"
+  // "ab�c"
+  // "�ab"
+  // "c�ab"
+  // "abc"
+  // "ab😄c"
+  
