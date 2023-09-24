d5128c6facdf1997f3bcc75ebb95081b3a7a2934
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 07:19:43 +0000

    repeat method

---
+++ 01_Basic/String/repeat_method.js
@@ -0,0 +1,7 @@
+//In summary, repeat() is used to repeat a string a specified number of times, while padEnd() is used to add characters to the end of a string until it reaches a specified length. They serve different purposes and are used in different scenarios depending on your requirements.
+
+//The repeat() method of String values constructs and returns a new string which contains the specified number of copies of this string, concatenated together.
+
+let str="Hello !"
+
+console.log(str.repeat(3));
