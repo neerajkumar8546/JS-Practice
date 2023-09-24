7db151bbb490133778c252f4efd1beca17ea2b2b
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 13:58:58 +0000

    trim start method

---
+++ 01_Basic/String/trimStart_method.js
@@ -0,0 +1,4 @@
+//The trimStart() method of String values removes whitespace from the beginning of this string and returns a new string, without modifying the original string. trimLeft() is an alias of this method.
+
+const greeting = '   Hello world!   ';
+console.log(`Start Trim : ${greeting.trimStart()}`);
