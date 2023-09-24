6ed20edf8ee734da4caa3545128923c7024d1bf5
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 07:03:32 +0000

    raw method

---
+++ 01_Basic/String/raw_method.js
@@ -0,0 +1,11 @@
+// It's used to get the raw string form of template literals — that is, substitutions 
+// (e.g. ${foo}) are processed, but escape sequences (e.g. \n) are not.
+
+const name = "Alice";
+const age = 30;
+
+const rawString = String.raw`Hello, my name is ${name}.\nI am ${age} years old.`;
+const rawString2 =`Hello, my name is ${name}.\nI am ${age} years old.`;
+
+console.log(rawString)
+console.log(rawString2)
