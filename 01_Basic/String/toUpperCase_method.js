c81821acc725fd76e4e441570805ff3c8159f3c0
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 13:21:06 +0000

    toUpperCase method

---
+++ 01_Basic/String/toUpperCase_method.js
@@ -0,0 +1,5 @@
+// The toUpperCase() method of String values returns this string converted to uppercase.
+
+let sentence='The quick brown fox jumps over the lazy dog.'
+
+console.log(sentence.toUpperCase());
