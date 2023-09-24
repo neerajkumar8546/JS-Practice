42ac9246eb73fa5896084463e9764c614afd2f52
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 13:19:23 +0000

    toLowerCase Method

---
+++ 01_Basic/String/toLowerCase_method.js
@@ -0,0 +1,5 @@
+//The toLowerCase() method of String values returns this string converted to lower case.
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toLowerCase());
