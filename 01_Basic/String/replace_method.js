b74edbbcee0858d15e9b6160e30f20abd7536cb4
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 07:59:43 +0000

    replace method

--- 01_Basic/String/replace_method.js
+++ 01_Basic/String/replace_method.js
@@ -1,4 +1,6 @@
-//The replace() method of String values returns a new string with one, some, or all matches of a pattern replaced by a replacement. The pattern can be a string or a RegExp, and the replacement can be a string or a function called for each match. If pattern is a string, only the first occurrence will be replaced. The original string is left unchanged.
+//The replace() method of String values returns a new string with one, some, or all matches of a pattern replaced by a replacement.
+// The pattern can be a string or a RegExp, and the replacement can be a string or a function called for each match.
+// If pattern is a string, only the first occurrence will be replaced. The original string is left unchanged.
 
 let p='The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?'
 
@@ -6,3 +8,4 @@ console.log(p.replace('dog','monkey'));
 // Expected output: The quick brown fox jumps over the lazy monkey. If the dog reacted, was it really lazy?
 const regexp=/dog/g
 console.log(p.replace(regexp,'Monkey'));
+// replace all string if using regular expression but using string replace only first occurence string is replaced.
