609daf319cf6c49972308d44177628992a837ca2
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-24 13:56:43 +0000

    trim end method

---
+++ 01_Basic/String/trimEnd_method.js
@@ -0,0 +1,7 @@
+//The trimEnd() method of String values removes whitespace from the end of this string and 
+//returns a new string, without modifying the original string. trimRight() is an alias of this 
+//method.
+
+const greeting = '   Hello world!   ';
+
+console.log(`End Trimed String:${greeting.trimEnd()}`);
