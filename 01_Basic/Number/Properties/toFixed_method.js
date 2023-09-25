f80c03205993b503bb4fb73761cc1d175ef63047
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-25 16:26:23 +0000

    toFixed method

---
+++ 01_Basic/Number/Properties/toFixed_method.js
@@ -0,0 +1,16 @@
+//The toFixed() method of Number values formats this number using fixed-point notation.
+
+function financial(x) {
+    return Number.parseFloat(x).toFixed(2);
+  }
+  
+  console.log(financial(123.456));
+  // Expected output: "123.46"
+  
+  console.log(financial(0.004));
+  // Expected output: "0.00"
+  
+  console.log(financial('1.23e+5'));
+  // Expected output: "123000.00"
+
+  
