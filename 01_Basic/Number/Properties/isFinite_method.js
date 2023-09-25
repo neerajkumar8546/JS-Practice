d4fb0471d415c9adeb81ad174ee115f219246fdb
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-25 15:09:08 +0000

    isFinite method

---
+++ 01_Basic/Number/Properties/isFinite_method.js
@@ -0,0 +1,8 @@
+console.log(Number.isFinite(1 / 0));
+// Expected output: false
+
+console.log(Number.isFinite(10 / 5));
+// Expected output: true
+
+console.log(Number.isFinite(0 / 0));
+// Expected output: false
