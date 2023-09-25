d18a0753a7157d52c8f6eda786bc89f50a0bb3b8
Neeraj kumar <neerajkumar8546@gmail.com>
2023-09-25 16:34:50 +0000

    toPrecision method

---
+++ 01_Basic/Number/Properties/toPrecision_method.js
@@ -0,0 +1,5 @@
+//The toPrecision() method of Number values returns a string representing this number to the specified precision.
+
+let number=123.556
+console.log(number.toPrecision(4));
+console.log(typeof(number.toPrecision(4)));
