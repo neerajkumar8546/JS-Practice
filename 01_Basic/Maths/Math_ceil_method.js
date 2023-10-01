19a75090324305f32d6ef806b50022a5df2bdfb7
Neeraj kumar <neerajkumar8546@gmail.com>
2023-10-01 12:54:25 +0000

    ceil method in maths

---
+++ 01_Basic/Maths/Math_ceil_method.js
@@ -0,0 +1,15 @@
+//The Math.ceil() static method always rounds up and returns the smallest integer greater than or equal to a given number.
+
+console.log(Math.ceil(0.95));
+// Expected output: 1
+
+console.log(Math.ceil(4));
+// Expected output: 4
+
+console.log(Math.ceil(7.004));
+// Expected output: 8
+
+console.log(Math.ceil(-7.004));
+// Expected output: -7
+
+console.log(Math.ceil(1.1));
