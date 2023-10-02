fb0d1adba2b943eb6eef86c85e968b8ec04cb9fe
Neeraj kumar <neerajkumar8546@gmail.com>
2023-10-02 17:52:02 +0000

    Array in js

---
+++ Array/array.js
@@ -0,0 +1,9 @@
+const number=[1,3,4,5,6,7,43,35,5,65,6,,33]
+
+const heros=['Tony Stark','Allu Arjun','nagarjun','Prabhash']
+
+const MyDetails=new Array('Neeraj',25,'B.tech','M',8317076281)
+
+console.log(number)
+console.log(heros)
+console.log(MyDetails)
