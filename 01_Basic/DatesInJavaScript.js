a4e2d7ac8d410ddefac1fa9daba3fb82db610f1d
Neeraj kumar <neerajkumar8546@gmail.com>
2023-10-02 17:05:30 +0000

    Date & Time in javaScript

--- 01_Basic/DatesInJavaScript.js
+++ 01_Basic/DatesInJavaScript.js
@@ -19,3 +19,25 @@ console.log(`Date format using toLocaleString() : ${newDate.toLocaleString()}`);
 
 console.log(`Date format using toLocaleDateString() : ${newDate.toLocaleDateString()}`);
 //Expected output : 10/2/2023
+
+
+//++++++++++++++++++++++++++++++++++++Time++++++++++++++++++++++++++++
+let myTimeStamp=Date.now()
+let myCreatedDate=new Date("01-07-2023")
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(myTimeStamp/1000));
+
+let newDate2=new Date()
+console.log(newDate2);
+console.log(newDate2.getDay())
+
+console.log(newDate2.toLocaleString('default',{
+    //weekday:"short",  //Mon
+    //dateStyle:"full"  //Monday, October 2, 2023
+    //day:"2-digit"  //02
+    //dayPeriod:"narrow"  //in the afternoon
+    //era:"long"  //10/2/2023 Anno Domini, 5:01:56 PM
+    
+}
+));
