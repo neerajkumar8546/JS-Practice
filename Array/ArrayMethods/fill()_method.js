5e2212ba0f92ab9ec8b98b53a0167f390fce285f
Neeraj kumar <neerajkumar8546@gmail.com>
2023-10-08 11:46:43 +0000

    fill() method in js

---
+++ Array/ArrayMethods/fill()_method.js
@@ -0,0 +1,9 @@
+//The fill() method of Array instances changes all elements within a range of indices in an array to a static value. It returns the modified array.
+
+const array1 = [1, 2, 3, 4];
+
+console.log(array1.fill('N',1,4));
+
+console.log(array1);
+
+// change element range of indices to static value and change original array.
